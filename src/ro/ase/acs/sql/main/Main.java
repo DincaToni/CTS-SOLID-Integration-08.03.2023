4ae47dd3387385f733ea85f52c79f7b72abc5b29
Your Name <dinca.toni@gmail.com>
2023-03-09 19:08:25 +0200

    DataReader interface and DataRead class added and replaced initial readData method in main

--- src/ro/ase/acs/sql/main/Main.java
+++ src/ro/ase/acs/sql/main/Main.java
@@ -2,7 +2,9 @@ package ro.ase.acs.sql.main;
 
 import ro.ase.acs.sql.classes.AddData;
 import ro.ase.acs.sql.classes.BuildTable;
+import ro.ase.acs.sql.classes.DataRead;
 import ro.ase.acs.sql.interfaces.DataInserator;
+import ro.ase.acs.sql.interfaces.DataReader;
 import ro.ase.acs.sql.interfaces.TableCreator;
 
 import java.sql.Connection;
@@ -25,30 +27,13 @@ public class Main {
 
             DataInserator dataInserator = new AddData();
             dataInserator.insertData(connection);
-            readData(connection);
+
+            DataReader dataReader = new DataRead();
+            dataReader.readData(connection);
 
             connection.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-
-    private static void readData(Connection connection) throws SQLException {
-        String sqlSelect = "SELECT * FROM employees";
-        Statement statement = connection.createStatement();
-        ResultSet rs = statement.executeQuery(sqlSelect);
-        while(rs.next()) {
-            int id = rs.getInt("id");
-            System.out.println("id: " + id);
-            String name = rs.getString(2);
-            System.out.println("name: " + name);
-            String address = rs.getString("address");
-            System.out.println("address: " + address);
-            double salary = rs.getDouble("salary");
-            System.out.println("salary: " + salary);
-        }
-        rs.close();
-        statement.close();
     }
 }
